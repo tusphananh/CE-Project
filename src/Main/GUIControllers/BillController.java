12bf0c3848a4004e27723bd2b1fed00ea8be10c7
Tu Phan <tusphananh@gmail.com>
2020-11-24 18:24:15 +0700

    update

--- src/Main/GUIControllers/BillController.java
+++ src/Main/GUIControllers/BillController.java
@@ -1,12 +1,19 @@
 package Main.GUIControllers;
 
+import Main.Models.HotelManagement;
 import Main.Models.Navigation;
+import Main.Models.Room;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+
+import java.io.IOException;
 
 public class BillController {
     @FXML
@@ -17,6 +24,23 @@ public class BillController {
 
     @FXML
     private Text totalText;
+
+    @FXML
+    void initialize() throws IOException {
+        loadSelectedRoom();
+    }
+
+    private void loadSelectedRoom() throws IOException {
+        for (Room r: HotelManagement.selectedRoom
+             ) {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../fxml/Bill_Item.fxml"));
+            HBox bill_itemPane = fxmlLoader.load();
+            Bill_ItemController bill_itemController = fxmlLoader.getController();
+            bill_itemController.setRoom(r);
+            bill_itemController.setRoomLabel(r.getID());
+            vstack.getChildren().add(bill_itemPane);
+        }
+    }
 
     @FXML
     void confirm(ActionEvent event) {
--- src/Main/GUIControllers/Bill_ItemController.java
+++ src/Main/GUIControllers/Bill_ItemController.java
@@ -1,4 +1,72 @@
 package Main.GUIControllers;
 
+import Main.Models.HotelManagement;
+import Main.Models.Room;
+import Main.Models.Service;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
+
+import java.io.IOException;
+
 public class Bill_ItemController {
+    private Room room;
+    @FXML
+    private Label roomLabel;
+
+    @FXML
+    private FlowPane serviceFlowPane;
+
+    @FXML
+    private Button detailButton;
+
+    @FXML
+    private Button deleteButton;
+
+
+    @FXML
+    private ImageView detailImageView,deleteImageView;
+
+    @FXML
+    void initialize() throws IOException {
+        detailImageView.setImage(new Image(getClass().getResourceAsStream("/images/detail.png")));
+        deleteImageView.setImage(new Image(getClass().getResourceAsStream("/images/drop.png")));
+        loadServices();
+    }
+
+    @FXML
+    void delete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showDetail(ActionEvent event) {
+
+    }
+
+    private void loadServices() throws IOException {
+        for (Service s: HotelManagement.services
+        ) {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../fxml/ServiceButton.fxml"));
+            ToggleButton button = fxmlLoader.load();
+            ServiceButtonController serviceButtonController = fxmlLoader.getController();
+            serviceButtonController.service = s;
+            button.setText(s.getName());
+            serviceFlowPane.getChildren().add(button);
+        }
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public void setRoomLabel(String roomLabel) {
+        this.roomLabel .setText("Room " + roomLabel);
+    }
 }
